174bb2d24556563798c963861ddccf486f855e9d
Frances0397 <kayleigh0385@gmail.com>
2023-07-03 21:33:07 +0200

    added ImageViewer and Button components + icons

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+
+const placeholderImage = require('./assets/images/background-image.png');
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={placeholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label={"Choose a photo"} />
+        <Button label={"Use this photo"} />
+      </View>
+      < StatusBar style="auto" />
     </View>
   );
 }
@@ -16,5 +27,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
